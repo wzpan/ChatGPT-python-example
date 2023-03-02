5c5bdb7f8b04014531b3ba9dc56cd9dfa24737a4
josephpan <josephpan@tencent.com>
2023-03-02 12:18:17 +0800

    first commit.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ChatGPT-python-example
-演示如何使用5行代码调用ChatGPT的官方API
+
+演示如何使用 5 行代码调用 ChatGPT 的官方API。要求 Python 版本 > 3.6 。
+
+## 1. 安装依赖
+
+``` bash
+pip3 install openai
+```
+
+## 2. 执行demo
+
+``` bash
+python3 bot.py
+```
---
+++ bot.py
@@ -0,0 +1,13 @@
+import openai
+openai.api_key = "sk-XXXXXXXXXX"  # 你的 OpenAI API Key
+
+# list models
+models = openai.Model.list()
+
+# create a completion
+completion = openai.Completion.create(model="gpt-3.5-turbo", \
+                                       messages=[{"role": "user", "content": "What is the OpenAI mission?"}], \
+                                        api_base="https://api.openai.com/v1/chat")
+
+# print the completion
+print(completion.choices[0].message.content)
