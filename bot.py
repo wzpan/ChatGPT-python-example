e38480aca2e036b677a198fe595882326e8c6c0c
josephpan <josephpan@tencent.com>
2023-03-08 12:28:02 +0800

    使用 ChatCompletion 代替 Completion, 以精简掉 api_base 参数(#1)

--- bot.py
+++ bot.py
@@ -2,9 +2,8 @@ import openai
 openai.api_key = "sk-XXXXXXXXXX"  # 你的 OpenAI API Key
 
 # create a completion
-completion = openai.Completion.create(model="gpt-3.5-turbo", \
-                                       messages=[{"role": "user", "content": "What is the OpenAI mission?"}], \
-                                        api_base="https://api.openai.com/v1/chat")
+completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", \
+                                       messages=[{"role": "user", "content": "你还活着吗？"}])
 
 # print the completion
 print(completion.choices[0].message.content)
