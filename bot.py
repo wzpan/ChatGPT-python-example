c826ae029da00dbadc6d7bf7fe6059c8ef6ceeac
潘伟洲 <josephpan@tencent.com>
2023-03-08 12:29:38 +0800

    Update bot.py
    更换query

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ openai.api_key = "sk-XXXXXXXXXX"  # 你的 OpenAI API Key
 
 # create a completion
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", \
-                                       messages=[{"role": "user", "content": "你还活着吗？"}])
+                                       messages=[{"role": "user", "content": "如何评价《三体》这部小说？"}])
 
 # print the completion
 print(completion.choices[0].message.content)
